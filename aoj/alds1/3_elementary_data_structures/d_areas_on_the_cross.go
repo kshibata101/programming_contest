e01c1db77e6108a3108244417e8b01326863ec9c
Shibata Kazuki <kshibata101@gmail.com>
2019-04-28 11:12:54 +0900

    fix for execution time limit

--- aoj/alds1/3_elementary_data_structures/d_areas_on_the_cross.go
+++ aoj/alds1/3_elementary_data_structures/d_areas_on_the_cross.go
@@ -4,15 +4,8 @@ import (
 	"fmt"
 )
 
-type Pos struct {
-	X int
-	Y int
-}
-
-type Area struct {
-	X1 int
-	X2 int
-	Y int
+type Pod struct {
+	Pos int
 	Area int
 }
 
@@ -20,61 +13,47 @@ func main() {
 	var str string
 	fmt.Scan(&str)
 
-	poses := []Pos{Pos{0, 0}}
-	areas := []Area{}
+	downs := []int{}
+	pods := []Pod{}
+	sum := 0
 	
 	rs := []rune(str)
-	y := 0
-	for x := 1; x <= len(rs); x++ {
-		s := string(rs[x-1])
+	for i := 0; i < len(rs); i++ {
+		s := string(rs[i])
 		switch s {
 		case "\\":
-			y--
+			downs = append(downs, i)
 		case "/":
-			y++
-			under_area := 0
-			delete_area_index := -1
-			for i := len(poses) - 1; i >= 0; i-- {
-				pos := poses[i]
-				if pos.Y < y {
-					// areaをチェックして、いたら消す
-					for j := len(areas) - 1; j >= 0; j-- {
-						if pos.X == areas[j].X2 {
-							under_area += areas[j].Area
-							delete_area_index = j
-							break
-						}
-					}
-				} else if pos.Y == y {
-					if delete_area_index >= 0 {
-						areas = areas[0:delete_area_index]
-					}
-					poses = poses[0:i+1]
-					areas = append(areas, Area{pos.X, x, y, under_area + x - pos.X - 1})
-					break
+			if len(downs) <= 0 {
+				continue
+			}
+			down := downs[len(downs)-1]
+			downs = downs[0:len(downs)-1]
+
+			area := i - down
+			sum += area
+
+			for j := len(pods) - 1; j >= 0; j-- {
+				pod := pods[j]
+				if down < pod.Pos {
+					area += pod.Area
+					pods = pods[0:j]
 				} else {
-					// ないはず
 					break
 				}
 			}
+			pods = append(pods, Pod{down, area})
 		}
-		poses = append(poses, Pos{x, y})
 	}
 
-	a := 0
-	k := len(areas)
-	for i := 0; i < k; i++ {
-		a += areas[i].Area
-	}
-	fmt.Println(a)
-	fmt.Print(k)
-	if len(areas) > 0 {
-		fmt.Print(" ")
-		fmt.Print(areas[0].Area)
-		for i := 1; i < k; i++ {
+	fmt.Println(sum)
+	fmt.Print(len(pods))
+	if len(pods) > 0 {
+		for i := 0; i < len(pods); i++ {
 			fmt.Print(" ")
-			fmt.Print(areas[i].Area)
+			fmt.Print(pods[i].Area)
 		}
 	}
 	fmt.Println()
 }
+
